2bb6dd3125a40da19bed73acede4d9345f879093
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-27 12:23:38 +0530

    module-2 transactions foundation developed 27-01-26 12:22 pm

--- src/com/bank/app/model/Transaction.java
+++ src/com/bank/app/model/Transaction.java
@@ -1,5 +1,66 @@
 package com.bank.app.model;
+import java.time.LocalDateTime;
+import com.bank.app.enums.TransactionType;
 
 public class Transaction {
+	private String transactionId;
+	private TransactionType transactionType;
+	private double amount;
+	private LocalDateTime timestamp;
+	private boolean success;
+	private String failureReason;
+	private String relatedTransationId;
+	
+	public Transaction(String transactionId, TransactionType transactionType, double amount,
+			LocalDateTime timestamp, boolean success, String  failureReason, String relatedTransactionId){
+		
+		this.transactionId = transactionId;
+		this.transactionType = transactionType;
+		this.amount = amount;
+		this.failureReason = failureReason;
+		this.relatedTransationId = relatedTransactionId;
+		this.timestamp = timestamp;
+		this.success = success;
+	}
+	
+	public boolean isDebit() {
+		return transactionType == transactionType.DEBIT;
+	}
+	
+	public boolean isCredit() {
+		return transactionType == transactionType.CREDIT;
+	}
+	
+	public boolean isReversal() {
+		return transactionType == transactionType.REVERSAL;
+	}
 
+	public String getTransactionId() {
+        return transactionId;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getFailureReason() {
+        return failureReason;
+    }
+
+    public String getRelatedTransactionId() {
+        return relatedTransationId;
+    }	
+	
 }
