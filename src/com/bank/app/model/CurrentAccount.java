8646748ab3212f0cc47e7b7f3bae4a387f41271c
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-27 13:33:09 +0530

    module-2 current-Account foundation developed 27-01-26 1:32 pm

--- src/com/bank/app/model/CurrentAccount.java
+++ src/com/bank/app/model/CurrentAccount.java
@@ -1,5 +1,37 @@
 package com.bank.app.model;
 
-public class CurrentAccount {
+import com.bank.app.enums.AccountTypes;
 
+public abstract class CurrentAccount extends Account{
+	private double overDraftLimit;
+	
+	public CurrentAccount(
+			String accountNumber,
+			double minimumBalance,
+			double overDraftLimit
+			) {
+		super(accountNumber,AccountTypes.CURRENT,minimumBalance);
+		this.overDraftLimit = overDraftLimit;
+	}
+	
+	
+	@Override
+	public boolean canDebit(double amount) {
+		return balance-amount >= -overDraftLimit;
+	}
+	
+	public void applyCharges() {
+		if(balance < 0) {
+			double fee = 50.0;
+			balance -= fee;
+		}
+	}
+	
+	@Override
+	public void applyMonthlyRules() {
+		applyCharges();
+	}
+	 public double getOverDraftLimit() {
+		 return overDraftLimit;
+	 }
 }
