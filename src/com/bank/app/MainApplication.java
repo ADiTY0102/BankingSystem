c14081b0756f4cb420c212a496a9631defdd4c67
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-28 11:29:53 +0530

    module-2 with Repository finished @28-01-26 11:30 am

--- src/com/bank/app/MainApplication.java
+++ src/com/bank/app/MainApplication.java
@@ -2,12 +2,16 @@ package com.bank.app;
 
 import com.bank.app.model.Customer;
 import com.bank.app.model.SavingsAccount;
+import com.bank.app.repository.BankRepository;
 
 public class MainApplication {
 
     public static void main(String[] args) {
-        System.out.println("Mini Banking Transaction Processing System started.");
-
+    	
+    	//Testing Module 1 and Module 2
+    	
+    /*  System.out.println("Mini Banking Transaction Processing System started.");
+        BankRepository repository = new BankRepository();
         Customer customer = new Customer("AB001", "Aditya Binjagermath", "ABCDE1234F");
 
         SavingsAccount savings = new SavingsAccount("AB2202ILF45", 1000.0, 0.04);
@@ -20,5 +24,36 @@ public class MainApplication {
         System.out.println("Customer: " + customer.getName());
         System.out.println("Net worth: " + customer.calculateNetWorth());
         System.out.println("Has frozen accounts: " + customer.hasFrozeenAccounts());
+        
+     */  
+        
+    	//Testing Module 1 and Module 2 => Repository Implementation
+        
+         
+	    System.out.println("Mini Banking Transaction Processing System started.");
+
+        BankRepository repository = new BankRepository();
+
+        Customer customer = new Customer("AB22", "arpita baraskar", "ABCDE1234F");
+        SavingsAccount savings = new SavingsAccount("AB02", 2000.0, 0.04);
+
+        customer.addAccount(savings);
+
+        repository.saveCustomer(customer);
+        repository.saveAccount(savings);
+
+        savings.credit(50000.0);
+        savings.debit(2000.0);
+
+        repository.saveAccount(savings); // update stored account
+
+        repository.findCustomersById("AB22").ifPresent(c -> {
+            System.out.println("Customer from repository: " + c.getName());
+            System.out.println("Net worth from repository: " + c.calculateNetWorth());
+        });
+
+        repository.findAccountsByNumber("AB02").ifPresent(a -> {
+            System.out.println("Account from repository balance: " + a.getBalance());
+        });
     }
 }
--- src/com/bank/app/repository/BankRepository.java
+++ src/com/bank/app/repository/BankRepository.java
@@ -1,5 +1,61 @@
 package com.bank.app.repository;
 
-public class BankRepository {
+import java.util.*;
 
+import com.bank.app.model.Account;
+import com.bank.app.model.Customer;
+import com.bank.app.model.Transaction;
+
+
+public class BankRepository {
+	
+	private Map<String,Customer> customersById = new HashMap<>();
+	private Map<String,Account> accountByNumber = new HashMap<>();
+	private List<Transaction> allTransactions = new ArrayList<>();
+	
+	
+	//customer operations//
+	
+	public void saveCustomer(Customer customer) {
+		if(customer == null) return;
+		customersById.put(customer.getCustomerId(), customer);
+	}
+	
+	public Optional<Customer>findCustomersById(String customerId){
+		if(customerId == null) {
+			return Optional.empty();
+		}
+		return Optional.ofNullable(customersById.get(customerId));
+	}
+	
+	public Collection<Customer> getAllCustomers(){
+		return Collections.unmodifiableCollection(customersById.values());
+	}
+	
+	
+	//Account Operations//
+	public void saveAccount(Account account) {
+		if(account == null) return;
+		accountByNumber.put(account.getAccountNumber(), account);
+	}
+	
+	public Optional<Account> findAccountsByNumber(String accountNumber){
+		if(accountNumber == null) {
+			return Optional.empty();
+		}
+		return Optional.ofNullable((accountByNumber.get(accountNumber)));
+	}
+	
+	public Collection<Account> getAllAccounts(){
+		return Collections.unmodifiableCollection(accountByNumber.values());
+	}
+	
+	//Transaction Operations // 
+	public void saveTransaction(Transaction transactions) {
+		if(transactions == null) return;
+		allTransactions.add(transactions);
+	}
+	public List<Transaction> getAllTransactions(){
+		return Collections.unmodifiableList(allTransactions);
+	}
 }
