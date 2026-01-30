fe363ccc01d73c44e0f45bafc0c48d444c16e103
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-30 10:58:06 +0530

    Converted BankRepository to Interface and implementing JDBC @30/01/2026 #2

--- src/com/bank/app/repository/BankRepository.java
+++ src/com/bank/app/repository/BankRepository.java
@@ -7,55 +7,72 @@ import com.bank.app.model.Customer;
 import com.bank.app.model.Transaction;
 
 
-public class BankRepository {
+public interface BankRepository {
 	
-	private Map<String,Customer> customersById = new HashMap<>();
-	private Map<String,Account> accountByNumber = new HashMap<>();
-	private List<Transaction> allTransactions = new ArrayList<>();
+	//Customer
+	void saveCustomer(Customer customer);
+	Optional<Customer> getCustomersById(String customerId);
 	
+	//Account
+	void saveAccount(Account account);
+	Optional<Account> getAccountsByNumber(String accountNumber);
+	List<Account> getAccountsByCustomerId(String customerId);
+	List<Account> getAllAccounts();
 	
-	//customer operations//
-	
-	public void saveCustomer(Customer customer) {
-		if(customer == null) return;
-		customersById.put(customer.getCustomerId(), customer);
-	}
-	
-	public Optional<Customer>findCustomersById(String customerId){
-		if(customerId == null) {
-			return Optional.empty();
-		}
-		return Optional.ofNullable(customersById.get(customerId));
-	}
-	
-	public Collection<Customer> getAllCustomers(){
-		return Collections.unmodifiableCollection(customersById.values());
-	}
-	
-	
-	//Account Operations//
-	public void saveAccount(Account account) {
-		if(account == null) return;
-		accountByNumber.put(account.getAccountNumber(), account);
-	}
-	
-	public Optional<Account> findAccountsByNumber(String accountNumber){
-		if(accountNumber == null) {
-			return Optional.empty();
-		}
-		return Optional.ofNullable((accountByNumber.get(accountNumber)));
-	}
-	
-	public Collection<Account> getAllAccounts(){
-		return Collections.unmodifiableCollection(accountByNumber.values());
-	}
-	
-	//Transaction Operations // 
-	public void saveTransaction(Transaction transactions) {
-		if(transactions == null) return;
-		allTransactions.add(transactions);
-	}
-	public List<Transaction> getAllTransactions(){
-		return Collections.unmodifiableList(allTransactions);
-	}
+	//Transaction
+	void saveTransaction(Transaction transaction);
+	List<Transaction> getTransactionsByAccount(String accountNumber);
+	List<Transaction> getAllTransactions();
 }
+
+/*
+private Map<String,Customer> customersById = new HashMap<>();
+private Map<String,Account> accountByNumber = new HashMap<>();
+private List<Transaction> allTransactions = new ArrayList<>();
+
+
+//customer operations//
+
+public void saveCustomer(Customer customer) {
+	if(customer == null) return;
+	customersById.put(customer.getCustomerId(), customer);
+}
+
+public Optional<Customer>findCustomersById(String customerId){
+	if(customerId == null) {
+		return Optional.empty();
+	}
+	return Optional.ofNullable(customersById.get(customerId));
+}
+
+public Collection<Customer> getAllCustomers(){
+	return Collections.unmodifiableCollection(customersById.values());
+}
+
+
+//Account Operations//
+public void saveAccount(Account account) {
+	if(account == null) return;
+	accountByNumber.put(account.getAccountNumber(), account);
+}
+
+public Optional<Account> findAccountsByNumber(String accountNumber){
+	if(accountNumber == null) {
+		return Optional.empty();
+	}
+	return Optional.ofNullable((accountByNumber.get(accountNumber)));
+}
+
+public Collection<Account> getAllAccounts(){
+	return Collections.unmodifiableCollection(accountByNumber.values());
+}
+
+//Transaction Operations // 
+public void saveTransaction(Transaction transactions) {
+	if(transactions == null) return;
+	allTransactions.add(transactions);
+}
+public List<Transaction> getAllTransactions(){
+	return Collections.unmodifiableList(allTransactions);
+}
+*/
