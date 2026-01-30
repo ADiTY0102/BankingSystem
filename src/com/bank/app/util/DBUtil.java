e3f64b38a831c8eab1c87dd97e49cdfc05bd7597
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-30 11:10:23 +0530

    Cveated DBUtil and implemented connection logic JDBC Driver Connections @30/01/2026 #3

---
+++ src/com/bank/app/util/DBUtil.java
@@ -0,0 +1,24 @@
+package com.bank.app.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	private static final String url = "jdbc:mysql://127.0.0.1:3306/bankdb";
+	private static final String user = "root";
+	private static final String password = "Aditya@2202";
+	
+	
+	static {
+		try {
+			Class.forName("com.mysl.cj.jdbc.Driver");
+		}catch(ClassNotFoundException e){
+			System.out.println("Error:" + e);
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(url,user,password);
+	}
+}
